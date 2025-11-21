4d468b2bf223caf75ab1f5962a41f965e12ca6f8
alessandro miele <alessandromiele94@hotmail.it>
2025-11-21 16:24:57 +0100

    feat: added shacdn and shared folder

--- app/backoffice-fe/src/App.vue
+++ app/backoffice-fe/src/App.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from '@shared/components/ui/button/Button.vue'
+</script>
 
 <template>
-  <h1 class="">BACKOFFICE</h1>
+  <h1>BACKOFFICE</h1>
+  <Button>Click me</Button>
 </template>
 
 <style scoped></style>
